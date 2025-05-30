294546871fc7674ce178d9c00f05d3eaa33f3d54
Rohit <rohit@lightci.com>
2025-05-30 12:42:45 -0600

    update keys for db config

--- apps/api/src/config/database.config.ts
+++ apps/api/src/config/database.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_DATABASE || 'rohit_constellation',
+  host: process.env.host || 'localhost',
+  port: parseInt(process.env.port || '5432'),
+  username: process.env.username || 'postgres',
+  password: process.env.password || 'postgres',
+  database: process.env.dbname || 'rohit_constellation',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: false,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
